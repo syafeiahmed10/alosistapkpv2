1122b2d3b14d2943a653f0e29453ba45cd1fdf43
Gema Adhan <gemaadhan@gmail.com>
2021-03-31 06:08:07 +0700

    tambah ok masih butuh penyempurnaan

---
+++ function.php
@@ -0,0 +1,6 @@
+<?php
+
+// require_once __DIR__ . "/vendor/autoload.php";
+require __DIR__ . "/vendor/autoload.php";
+$mongoClient = new MongoDB\Client('mongodb://alosistapkpv2:permisitujuhtujuh@localhost:27017/alosistapkpv2');
+$collection = $mongoClient->alosistapkpv2->penanganan_pemukiman_kumuh;
