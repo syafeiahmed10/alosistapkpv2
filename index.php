30210dad8194df31381b925614fef8529d2c69ab
Gema Adhan <gemaadhan@gmail.com>
2021-03-31 06:07:45 +0700

    tambah ok masih butuh penyempurnaan

--- index.php
+++ index.php
@@ -1,10 +1,34 @@
 <?php
 
-require_once __DIR__ . "/vendor/autoload.php";
+include 'function.php';
+if (isset($_POST['submit'])) {
+
+    $insertOneResult = $collection->insertOne([
+        'kota' => $_POST['kota'],
+        'nama_lokasi' => $_POST['lokasi'],
+        'luas_tertangani' => $_POST['luas_tertangani'],
+        'lingkup_administratif' => [
+            'kecamatan' => $_POST['kecamatan'],
+            'kelurahan' => $_POST['kelurahan'],
+            'RW' => $_POST['rw'],
+            'RT' => $_POST['rt']
+        ],
+        'koordinat' => [
+            'lintang' => $_POST['lintang'],
+            'bujur' => $_POST['bujur']
+        ],
+        'tahun' => $_POST['tahun'],
+        'jenis_kegiatan' => $_POST['kegiatan'],
+        'sumber_dana' => $_POST['sumber_dana'],
+        'sisa_kumuh' => $_POST['sisa_kumuh'],
+    ]);
 
 
-$mongoClient = new MongoDB\Client('mongodb://alosistapkpv2:permisitujuhtujuh@localhost:27017/alosistapkpv2');
-$collection = $mongoClient->alosistapkpv2->penanganan_pemukiman_kumuh;
+    $_SESSION['success'] = "Import Berhasil";
+    header("Location: index.php");
+    var_dump($insertOneResult);
+}
+
 
 
 
@@ -25,7 +49,7 @@ $collection = $mongoClient->alosistapkpv2->penanganan_pemukiman_kumuh;
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link rel="apple-touch-icon" href="https://i.imgur.com/QRAUqs9.png">
-    <link rel="shortcut icon" href="https://i.imgur.com/QRAUqs9.png">
+    <link rel="shortcut icon" href="images/favicon.png">
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/normalize.css@8.0.0/normalize.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css">
@@ -41,6 +65,7 @@ $collection = $mongoClient->alosistapkpv2->penanganan_pemukiman_kumuh;
 
     <!-- <script type="text/javascript" src="https://cdn.jsdelivr.net/html5shiv/3.7.3/html5shiv.min.js"></script> -->
 
+
 </head>
 
 <body>
@@ -55,12 +80,16 @@ $collection = $mongoClient->alosistapkpv2->penanganan_pemukiman_kumuh;
                     <li class="menu-title">UI elements</li><!-- /.menu-title -->
 
                     <li class="menu-item-has-children active dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-table"></i>Tables</a>
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Tables</a>
                         <ul class="sub-menu children dropdown-menu">
                             <li><i class="fa fa-table"></i><a href="tables-basic.html">Basic Table</a></li>
                             <li><i class="fa fa-table"></i><a href="tables-data.html">Data Table</a></li>
                         </ul>
                     </li>
+                    <li>
+                        <a href="widgets.html"> Widgets </a>
+                    </li>
+
                 </ul>
             </div><!-- /.navbar-collapse -->
         </nav>
@@ -136,10 +165,159 @@ $collection = $mongoClient->alosistapkpv2->penanganan_pemukiman_kumuh;
 
                     <div class="col-md-12">
                         <div class="card">
-                            <div class="card-header">
-                                <strong class="card-title">Data Table</strong>
+                            <div class="card-header d-flex">
+                                <!-- <strong class="card-title">Data Table</strong> -->
+                                <button class="btn btn-success" data-toggle="modal" data-target=".tambahmodal">Tambah Data</button>
+                                <form action="" method="post" enctype="multipart/form-data">
+                                    <div class="modal fade tambahmodal" tabindex="-1" role="dialog" aria-labelledby="myExtraLargeModalLabel" aria-hidden="true">
+                                        <div class="modal-dialog modal-lg">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+
+                                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                        <span aria-hidden="true">&times;</span>
+                                                    </button>
+                                                    <h3 class="modal-title" id="exampleModalLabel">Penanganan Kawasan Kumuh</h3>
+                                                </div>
+                                                <div class="modal-body">
+
+
+                                                    <div class="container-fluid">
+                                                        <div class="row">
+                                                            <div class="col-md-6">
+                                                                <div class="form-group">
+                                                                    <label class=" form-control-label">Kota/Kabupaten</label>
+                                                                    <div class="input-group">
+                                                                        <div class="input-group-addon"><i class="fa fa-calendar"></i></div>
+                                                                        <input class="form-control" type="text" id="kota" name="kota">
+                                                                    </div>
+                                                                    <small class="form-text text-muted">ex. semarang</small>
+                                                                </div>
+                                                                <div class="form-group">
+                                                                    <label class=" form-control-label">Lokasi</label>
+                                                                    <div class="input-group">
+                                                                        <div class="input-group-addon"><i class="fa fa-calendar"></i></div>
+                                                                        <input class="form-control" type="text" id="lokasi" name="lokasi">
+                                                                    </div>
+                                                                    <small class="form-text text-muted">ex. rowosari</small>
+                                                                </div>
+                                                                <div class="form-group">
+                                                                    <label class=" form-control-label">Luas</label>
+                                                                    <div class="input-group">
+                                                                        <div class="input-group-addon"><i class="fa fa-calendar"></i></div>
+                                                                        <input class="form-control" type="text" id="luas_tertangani" name="luas_tertangani">
+                                                                    </div>
+                                                                    <small class="form-text text-muted">ex. 3</small>
+                                                                </div>
+                                                                <div class="form-group">
+                                                                    <label class=" form-control-label">Kecamatan</label>
+                                                                    <div class="input-group">
+                                                                        <div class="input-group-addon"><i class="fa fa-calendar"></i></div>
+                                                                        <input class="form-control" type="text" id="kecamatan" name="kecamatan">
+                                                                    </div>
+                                                                    <small class="form-text text-muted">ex. tembalang</small>
+                                                                </div>
+                                                                <div class="form-group">
+                                                                    <label class=" form-control-label">kelurahan</label>
+                                                                    <div class="input-group">
+                                                                        <div class="input-group-addon"><i class="fa fa-calendar"></i></div>
+                                                                        <input class="form-control" type="text" id="kelurahan" name="kelurahan">
+                                                                    </div>
+                                                                    <small class="form-text text-muted">ex. meteseh</small>
+                                                                </div>
+                                                                <div class="form-group">
+                                                                    <label class=" form-control-label">rw</label>
+                                                                    <div class="input-group">
+                                                                        <div class="input-group-addon"><i class="fa fa-calendar"></i></div>
+                                                                        <input class="form-control" type="text" id="rw" name="rw">
+                                                                    </div>
+                                                                    <small class="form-text text-muted">ex. 1</small>
+                                                                </div>
+                                                            </div>
+                                                            <div class="col-md-6">
+                                                                <div class="form-group">
+                                                                    <label class=" form-control-label">rt</label>
+                                                                    <div class="input-group">
+                                                                        <div class="input-group-addon"><i class="fa fa-calendar"></i></div>
+                                                                        <input class="form-control" type="text" id="rt" name="rt">
+                                                                    </div>
+                                                                    <small class="form-text text-muted">ex. 12</small>
+                                                                </div>
+                                                                <div class="form-group">
+                                                                    <label class=" form-control-label">Lintang</label>
+                                                                    <div class="input-group">
+                                                                        <div class="input-group-addon"><i class="fa fa-calendar"></i></div>
+                                                                        <input class="form-control" type="text" id="lintang" name="lintang">
+                                                                    </div>
+                                                                    <small class="form-text text-muted">ex. 80</small>
+                                                                </div>
+                                                                <div class="form-group">
+                                                                    <label class=" form-control-label">Bujur</label>
+                                                                    <div class="input-group">
+                                                                        <div class="input-group-addon"><i class="fa fa-calendar"></i></div>
+                                                                        <input class="form-control" type="text" id="bujur" name="bujur">
+                                                                    </div>
+                                                                    <small class="form-text text-muted">ex. 70</small>
+                                                                </div>
+                                                                <div class="form-group">
+                                                                    <label class=" form-control-label">Tahun</label>
+                                                                    <div class="input-group">
+                                                                        <div class="input-group-addon"><i class="fa fa-calendar"></i></div>
+                                                                        <input class="form-control" type="text" id="tahun" name="tahun">
+                                                                    </div>
+                                                                    <small class="form-text text-muted">ex. 2021</small>
+                                                                </div>
+                                                                <div class="form-group">
+                                                                    <label class=" form-control-label">Kegiatan</label>
+                                                                    <div class="input-group">
+                                                                        <div class="input-group-addon"><i class="fa fa-calendar"></i></div>
+                                                                        <input class="form-control" type="text" id="kegiatan" name="kegiatan">
+                                                                    </div>
+                                                                    <small class="form-text text-muted">ex. Pengelolaan Sampah</small>
+                                                                </div>
+                                                                <div class="form-group">
+                                                                    <label class=" form-control-label">Sumber Dana</label>
+                                                                    <div class="input-group">
+                                                                        <div class="input-group-addon"><i class="fa fa-calendar"></i></div>
+                                                                        <input class="form-control" type="text" id="sumber_dana" name="sumber_dana">
+                                                                    </div>
+                                                                    <small class="form-text text-muted">ex. kotaku</small>
+                                                                </div>
+                                                                <div class="form-group">
+                                                                    <label class=" form-control-label">Sisa Kumuh</label>
+                                                                    <div class="input-group">
+                                                                        <div class="input-group-addon"><i class="fa fa-calendar"></i></div>
+                                                                        <input class="form-control" type="text" id="sisa_kumuh" name="sisa_kumuh">
+                                                                    </div>
+                                                                    <small class="form-text text-muted">ex. 4</small>
+                                                                </div>
+                                                            </div>
+
+
+                                                        </div>
+                                                    </div>
+
+
+
+
+
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                                    <button type="submit" name="submit" class="btn btn-primary">Tambah</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </form>
+
+
+
+
                             </div>
                             <div class="card-body">
+
                                 <table id="bootstrap-data-table" class="table table-striped table-bordered">
                                     <thead>
                                         <tr>
@@ -157,6 +335,12 @@ $collection = $mongoClient->alosistapkpv2->penanganan_pemukiman_kumuh;
                                             <th>Kegiatan</th>
                                             <th>Sumber Dana</th>
                                             <th>Sisa Kumuh</th>
+
+                                            <th></th>
+                                            <!-- <th></th> -->
+
+
+
                                         </tr>
                                     </thead>
 
@@ -181,6 +365,14 @@ $collection = $mongoClient->alosistapkpv2->penanganan_pemukiman_kumuh;
                                                 <td><?php echo $data->jenis_kegiatan ?></td>
                                                 <td><?php echo $data->sumber_dana ?></td>
                                                 <td><?php echo $data->sisa_kumuh ?></td>
+
+
+                                                <td>
+                                                    <div class="btn-group">
+                                                        <button type="button" class="fa fa-pencil btn btn-warning btn-sm baris"></button>
+                                                        <button type="button" class="fa fa-trash-o btn btn-danger btn-sm baris"></button>
+                                                    </div>
+                                                </td>
                                             </tr>
                                             <?php $i++; ?>
                                         <?php endforeach; ?>
